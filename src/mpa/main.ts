793822dd36a8b7d0f4ac50b7e0803b625bac45ea
Virtual Jasper <jasper2virtual@hotmail.com>
2023-12-19 23:35:07 +0800

    go to sleep

--- src/mpa/main.ts
+++ src/mpa/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './style.scss'
+import App from './AnsIndex.vue'
 
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {resolve} from 'path'
+
+const root=resolve(__dirname,'src','mpa')
+const outDir=resolve(__dirname,'docs')
+const assets=resolve(__dirname,'src','assets')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+            '@root': root,
+            '@assets': assets
+        },
+    },
+    plugins: [vue()],
+    root,
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(root,'index.html'),
+                about: 'about.html',
+            },
+        },
+        outDir,
+        emptyOutDir: true,
+    },
 })
